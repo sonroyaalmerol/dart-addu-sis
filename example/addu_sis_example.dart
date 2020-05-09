7582d9f7ffc98964c6ba871c6309376521ed63db
sonroyaalmerol <sonroya.almerol@gmail.com>
2020-05-09 15:43:44 +0800

    remove credentials from example

--- example/addu_sis_example.dart
+++ example/addu_sis_example.dart
@@ -1,6 +1,6 @@
 import 'package:addu_sis/addu_sis.dart';
 void main() async {
-  var sis = SIS('sonraalmerol', 'AdDU2201901285413');
+  var sis = SIS('username', 'password');
   print((await sis.getUser()).all());
   print((await sis.getCurriculum()).all());
   print((await sis.getRegistration()).all());
